3ff3d2a18e8d8fbb13f8ba231a177e746d488ddb
Vendicated <vendicated@riseup.net>
2021-04-29 01:31:57 +0200

    Oops :3

--- src/commands/test.ts
+++
@@ -1,8 +0,0 @@
-import { InbuiltCommand } from "../classes";
-
-export default class Exit extends InbuiltCommand {
-  declare usage: "exit CODE";
-  invoke() {
-    return { out: this.getVariable(this.args[0]), code: 0 };
-  }
-}
